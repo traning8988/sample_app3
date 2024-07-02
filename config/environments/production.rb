04b3f303c848e650624dbb7fdca39952d326c2ac
Shota Nakamichi <s02178906@gmail.com>
2024-07-02 07:56:36 +0000

    peoduction.rbにrenderからのアクセス許可を追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.hosts << 'sample-app3-xhxk.onrender.com'
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com

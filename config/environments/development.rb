d5128934ca470da34f152b3e0e4ce0385edce53f
Shota Nakamichi <s02178906@gmail.com>
2024-07-03 22:50:31 +0900

    "Add allowed host for production environment"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.hosts << "sample-app3-xhxk.onrender.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time

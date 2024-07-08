5a0e08bd90685a09115b8f4527b9128b7c99407e
Shota Nakamichi <s02178906@gmail.com>
2024-07-08 08:28:20 +0000

    Finish static-pages

--- config/initializers/host_authorization.rb
+++ config/initializers/host_authorization.rb
@@ -1,1 +1,1 @@
-Rails.application.config.hosts << "sample-app3-xhxk.onrender.com"
+Rails.application.config.hosts << 'sample-app3-xhxk.onrender.com'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'
+  get "/about" => 'static_pages#about'
+  root 'static_pages#home'
 end
